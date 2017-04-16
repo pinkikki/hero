69e7e65845382763471e48461fdb84af8d6e8506
mizutani_wataru <mizutani@aevic.co.jp>
2017-04-16 22:47:53 +0900

    リファクタリング

--- Assets/script/core/event/EventManager.cs
+++ Assets/script/core/event/EventManager.cs
@@ -2,80 +2,91 @@
 using System.Collections.Generic;
 using System.Linq;
 using script.common.dao;
-using script.common.entity;
+using script.core.audio;
 using script.core.monoBehaviour;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace script.core.@event
 {
-    public class EventManager : SingletonMonoBehaviour<EventManager> {
-
-        [SerializeField]
-        String eventId;
+    public class EventManager : SingletonMonoBehaviour<EventManager>
+    {
         List<int> eventList;
         Dictionary<int, EventTask> eventDct = new Dictionary<int, EventTask>();
         int currentEvent;
         Mode eventMode;
-        enum Mode {
+
+        enum Mode
+        {
             Loading,
             WaitTrigger,
             ExecuteEvent,
             WaitEvent,
             End
         }
+
         float time;
-        void Start () {
+
+        void Start()
+        {
             eventMode = Mode.Loading;
             Load();
         }
 
-        void Update () {
-            if(Mode.WaitTrigger == eventMode) {
-                if ((amInstance == null || amInstance.isLoadComplete()) &&
-                    (ciInstance == null || ciInstance.isLoadComplete()) &&
-                    (oiInstance == null || oiInstance.isLoadComplete())) {
-                    if(eventList.Count > 0) {
-                        eventMode = Mode.ExecuteEvent;
-                        currentEvent = eventList[0];
-                    }
-                }
-
-            } else if(Mode.ExecuteEvent == eventMode) {
-                Execute();
-                eventMode = Mode.WaitEvent;
-
-            } else if(Mode.WaitEvent == eventMode) {
-                if(eventDct[currentEvent].TaskEndFlg) {
-                    currentEvent = 0;
-                    eventList.RemoveAt(0);
-                    eventMode = Mode.WaitTrigger;
-                } else if(!eventDct[currentEvent].StopFlg) {
+        void Update()
+        {
+            switch (eventMode)
+            {
+                case Mode.WaitTrigger:
+                    if ((!AudioManager.Exist() && !AudioManager.Instance.IsLoadComplete()) ||
+                        (ciInstance != null && !ciInstance.isLoadComplete()) ||
+                        (oiInstance != null && !oiInstance.isLoadComplete())) return;
+                    if (eventList.Count <= 0) return;
                     eventMode = Mode.ExecuteEvent;
-                }
-            }
-        }
-
-        void FixedUpdate() {
-            if(Mode.WaitEvent == eventMode) {
-                if(eventDct[currentEvent].StopFlg) {
-                    float sleepTime = eventDct[currentEvent].SleepTime;
-                    if (0.0f < sleepTime) {
-                        time += Time.deltaTime;
-                        if (sleepTime < time) {
-                            time = 0.0f;
-                            eventDct[currentEvent].SleepTime = 0.0f;
-                            eventDct[currentEvent].StopFlg = false;
-                        }
+                    currentEvent = eventList[0];
+                    break;
+                case Mode.ExecuteEvent:
+                    Execute();
+                    eventMode = Mode.WaitEvent;
+                    break;
+                case Mode.WaitEvent:
+                    if (eventDct[currentEvent].TaskEndFlg)
+                    {
+                        currentEvent = 0;
+                        eventList.RemoveAt(0);
+                        eventMode = Mode.WaitTrigger;
                     }
-                }
+                    else if (!eventDct[currentEvent].StopFlg)
+                    {
+                        eventMode = Mode.ExecuteEvent;
+                    }
+                    break;
+                case Mode.Loading:
+                    break;
+                case Mode.End:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
 
-        void Load() {
+        void FixedUpdate()
+        {
+            if (Mode.WaitEvent != eventMode) return;
+            if (!eventDct[currentEvent].StopFlg) return;
+            var sleepTime = eventDct[currentEvent].SleepTime;
+            if (!(0.0f < sleepTime)) return;
+            time += Time.deltaTime;
+            if (!(sleepTime < time)) return;
+            time = 0.0f;
+            eventDct[currentEvent].SleepTime = 0.0f;
+            eventDct[currentEvent].StopFlg = false;
+        }
 
-            String sceneId = SceneManager.GetActiveScene().name;
-            List<EventEntity> entityList = EventDao.SelectBySceneId(sceneId);
+        void Load()
+        {
+            var sceneId = SceneManager.GetActiveScene().name;
+            var entityList = EventDao.SelectBySceneId(sceneId);
 
             entityList.ForEach(e => eventDct[e.EventId] = new EventTask(EventDetailDao.SelectByEventId(e.EventId)));
             eventList = entityList.Select(e => e.EventId).ToList();
@@ -83,15 +94,18 @@ namespace script.core.@event
             eventMode = Mode.WaitTrigger;
         }
 
-        void Execute() {
+        void Execute()
+        {
             eventDct[currentEvent].Execute();
         }
 
-        public void NextTask() {
+        public void NextTask()
+        {
             eventDct[currentEvent].StopFlg = false;
         }
 
-        public void SkipEvent(int skipNum) {
+        public void SkipEvent(int skipNum)
+        {
             eventDct[currentEvent].CurrentIndex += skipNum;
         }
     }
