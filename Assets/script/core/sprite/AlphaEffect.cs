cf5313750299a1db6d16b68a70a797b0716d3d38
mizutani_wataru <mizutani@aevic.co.jp>
2017-05-21 18:48:28 +0900

    spriteモジュールを追加

---
+++ Assets/script/core/sprite/AlphaEffect.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace script.core.sprite
+{
+    public class AlphaEffect : MonoBehaviour
+    {
+        [SerializeField] int effectWaitNum = 50;
+        [SerializeField] int currentEffectWaitNum;
+        float alphaValue = 1.0f;
+        int direction = -1;
+
+        void Start()
+        {
+        }
+
+        void Update()
+        {
+        }
+
+        void FixedUpdate()
+        {
+            if (currentEffectWaitNum < 1)
+            {
+                if (direction > 0 && 1.0f <= alphaValue)
+                {
+                    direction = -1;
+                }
+                else if (direction < 0 && 0.0f >= alphaValue)
+                {
+                    direction = 1;
+                }
+                alphaValue += Time.deltaTime * direction;
+                GetComponent<Image>().color = new Color(1, 1, 1, alphaValue);
+
+                if (1.0f <= alphaValue)
+                {
+                    currentEffectWaitNum = effectWaitNum;
+                }
+            }
+            else
+            {
+                currentEffectWaitNum--;
+            }
+        }
+    }
+}
