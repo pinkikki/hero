6758528d7cd5b710370d485a8cfe76ba91931024
pinkikki <w-mizutani@pinkikki.jp>
2018-02-04 19:37:33 +0900

    セピア色でシーンを流す対応

---
+++ Assets/script/logic/ending/PostEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffect : MonoBehaviour {
+
+	public Material sepia;
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		Graphics.Blit (src, dest, sepia);
+	}
+}
