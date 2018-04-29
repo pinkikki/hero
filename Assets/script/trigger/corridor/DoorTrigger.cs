6fc777327c9fb973940b4852357a39b0a3eda2a1
pinkikki <w-mizutani@pinkikki.jp>
2018-04-29 19:24:35 +0900

    add door event of corridor

--- Assets/script/trigger/corridor/DoorTrigger.cs
+++ Assets/script/trigger/corridor/DoorTrigger.cs
@@ -16,8 +16,36 @@ namespace script.trigger.corridor
 		void OnCollisionEnter2D(Collision2D other) {
 			if (other.gameObject.name == "yusuke")
 			{
-				SceneStatus.EntranceNo = 1;
-				SceneLoadManager.Instance.LoadLevelInLoading(1.0f, "artroom", null);
+				if (gameObject.name == "door_artroom")
+				{
+					SceneStatus.EntranceNo = 1;
+					SceneLoadManager.Instance.LoadLevelInLoading(1.0f, "artroom", null);
+				}
+				else if (gameObject.name == "door_a")
+				{
+					SceneStatus.EntranceNo = 1;
+					SceneLoadManager.Instance.LoadLevelInLoading(1.0f, "classroom", null);
+				}
+				else if (gameObject.name == "door_b")
+				{
+					SceneStatus.EntranceNo = 2;
+					SceneLoadManager.Instance.LoadLevelInLoading(1.0f, "classroom", null);
+				}
+				else if (gameObject.name == "door_c_1")
+				{
+					SceneStatus.EntranceNo = 1;
+					SceneLoadManager.Instance.LoadLevelInLoading(1.0f, "schoolyard", null);
+				}
+				else if (gameObject.name == "door_c_2")
+				{
+					SceneStatus.EntranceNo = 2;
+					SceneLoadManager.Instance.LoadLevelInLoading(1.0f, "schoolyard", null);
+				}
+				else if (gameObject.name == "door_c_3")
+				{
+					SceneStatus.EntranceNo = 3;
+					SceneLoadManager.Instance.LoadLevelInLoading(1.0f, "schoolyard", null);
+				}
 			}
 		}
 	}
